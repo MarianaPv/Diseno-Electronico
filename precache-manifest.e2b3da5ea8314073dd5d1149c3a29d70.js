11b87c718234c549d43b63a66c24401ec4fb791b
Mariana <marianapertuz@uninorte.edu.co>
2020-02-29 19:25:30 -0500

    Updates

---
+++ precache-manifest.e2b3da5ea8314073dd5d1149c3a29d70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2d7f71031252b906f9149d04ba067c",
+    "url": "/Diseno-Electronico/index.html"
+  },
+  {
+    "revision": "5a851eb686c581dfb45e",
+    "url": "/Diseno-Electronico/static/css/2.b756e824.chunk.css"
+  },
+  {
+    "revision": "d62555763d3c3709ad02",
+    "url": "/Diseno-Electronico/static/css/main.f975c4fd.chunk.css"
+  },
+  {
+    "revision": "5a851eb686c581dfb45e",
+    "url": "/Diseno-Electronico/static/js/2.83e03eb9.chunk.js"
+  },
+  {
+    "revision": "ac5d9a2ccd3ba848587d2a460762f2f0",
+    "url": "/Diseno-Electronico/static/js/2.83e03eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d62555763d3c3709ad02",
+    "url": "/Diseno-Electronico/static/js/main.d8d01b68.chunk.js"
+  },
+  {
+    "revision": "083e3caa6b45d7601e35",
+    "url": "/Diseno-Electronico/static/js/runtime-main.14789d3c.js"
+  }
+]);
