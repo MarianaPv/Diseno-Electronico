99e0b99804b288e527a8f5aab093bb1ca0aa9abe
Mariana <marianapertuz@uninorte.edu.co>
2020-03-27 15:01:02 -0500

    sql actualizado

--- Backend/index.js
+++ Backend/index.js
@@ -2,9 +2,25 @@ const dgram = require("dgram");
 const server = dgram.createSocket("udp4");
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 const cors = require("cors");
 
+const SELECT_ALL_TRUCKS = "SELECT * FROM camiones";
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "camiones",
+  password: "password"
+});
+
 let coordinates;
+let message;
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Node connected ");
+});
 
 server.on("error", err => {
   console.log(err.stack);
@@ -12,9 +28,32 @@ server.on("error", err => {
 });
 
 server.on("message", (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
   coordinates = msg;
+  message = msg.toString();
+  const plusSign = message.indexOf("+");
+  const minusSign = message.indexOf("-");
+  const latitud =
+    message.substring(plusSign + 1, plusSign + 3) +
+    "." +
+    message.substring(plusSign + 3, plusSign + 8);
+  const longitud =
+    "-" +
+    message.substring(minusSign + 2, minusSign + 4) +
+    "." +
+    message.substring(minusSign + 4, minusSign + 9);
+  const janTimeMilli = new Date("January 5, 1980 19:00:00").getTime();
+  const numWeeks = parseInt(message.substring(6, 10));
+  const numSeconds = parseInt(message.substring(11, 16));
+  const numWeeksMilli = numWeeks * 7 * 24 * 60 * 60 * 1000;
+  const dateWeek = parseInt(message.substring(10, 11)) * 24 * 60 * 60 * 1000;
+  const time = numSeconds * 1000 + numWeeksMilli + janTimeMilli + dateWeek;
+  const time1 = new Date(time).toString();
+  connection.query(`INSERT INTO camiones (latitud, longitud, date)
+  VALUES ('${latitud}', '${longitud}', '${time1}')
+  `);
+  console.log(`server got: ${time1} from ${rinfo.address}:${rinfo.port}`);
 });
+
 server.on("listening", () => {
   const address = server.address();
   console.log(`server listening ${address.address}:${address.port}`);
@@ -28,15 +67,24 @@ app.use(function(req, res, next) {
   );
   next();
 });
+
 app.get("/", (req, res) => {
   res.send("go to /coords to see info");
 });
 
 app.get("/coords", (req, res) => {
-  res.json(`${coordinates}`);
+  connection.query(SELECT_ALL_TRUCKS, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data1: results
+      });
+    }
+  });
 });
 
-app.listen(80, () => {
+app.listen(5000, () => {
   console.log("Listening on Port 5000");
 });
-server.bind(5010);
+server.bind(5000);
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component, useState, useEffect, useRef } from "react";
 import "./App.css";
 import L from "leaflet";
 import userLocation from "./marker.png";
@@ -8,82 +8,87 @@ function App() {
   const [message, setMessage] = useState(
     ">REV002096113686+1101831-0748084000022732;ID=SyrusG4<"
   );
-  const [date, setDate] = useState("");
+
+  const [historic, setHistoric] = useState([]);
+  const [zoom, setZoom] = useState(15);
 
   const [color, setColor] = useState("black");
-  const plusSign = message.indexOf("+");
-  const minusSign = message.indexOf("-");
-  const latitud =
-    message.substring(plusSign + 1, plusSign + 3) +
-    "." +
-    message.substring(plusSign + 3, plusSign + 8);
-  const longitud =
-    "-" +
-    message.substring(minusSign + 2, minusSign + 4) +
-    "." +
-    message.substring(minusSign + 4, minusSign + 9);
-
-  const decomposeTime = dummy => {
-    const janTimeMilli = new Date("January 5, 1980 19:00:00").getTime();
-    const numWeeks = parseInt(dummy.substring(6, 10));
-    const numSeconds = parseInt(dummy.substring(11, 16));
-    const numWeeksMilli = numWeeks * 7 * 24 * 60 * 60 * 1000;
-    const dateWeek = parseInt(dummy.substring(10, 11)) * 24 * 60 * 60 * 1000;
-    const time = numSeconds * 1000 + numWeeksMilli + janTimeMilli + dateWeek;
-    const time1 = new Date(time).toString();
-    setDate(time1);
-  };
 
   useEffect(() => {
     getInfo();
     setInterval(() => {
       getInfo();
-    }, 10000);
+    }, 1000);
   }, []);
 
   useEffect(() => {
     setColor(color === "blue" ? "black" : "blue");
-    decomposeTime(message); // cambiar a message
-  }, [message]);
+  }, [historic]);
 
   const getInfo = () => {
-    fetch("http://bdd24ce8.ngrok.io/coords")
+    fetch("http://localhost:5000/coords")
       .then(res => res.json())
       .then(data => {
-        if (message !== data) {
-          setMessage(data);
+        let { data1 } = data;
+
+        if (historic.toString() !== data1.toString()) {
+          setHistoric(data1);
         }
       })
       .catch(err => console.log(err));
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vw" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vw"
+      }}
+    >
       <div style={{ color: color, fontWeight: "bolder" }}>{message}</div>
       <div style={{ color: color, fontWeight: "bolder" }}>
-        Latitud: {latitud}
+        Latitud: {historic.length > 0 && historic[historic.length - 1].latitud}
       </div>
       <div style={{ color: color, fontWeight: "bolder" }}>
-        Longitud: {longitud}
+        Longitud:{" "}
+        {historic.length > 0 && historic[historic.length - 1].longitud}
       </div>
       <div style={{ color: color, fontWeight: "bolder" }}>
-        Fecha y Hora: {date}
+        Fecha y Hora:{" "}
+        {historic.length > 0 && historic[historic.length - 1].date}
       </div>
       <div className="dive">
-        <Map className="map" center={[latitud, longitud]}>
+        <Map
+          className="map"
+          center={[
+            historic.length > 0 && historic[historic.length - 1].latitud,
+            historic.length > 0 && historic[historic.length - 1].longitud
+          ]}
+          zoom={zoom}
+          onZoomEnd={e => {
+            setZoom(e.target._zoom);
+          }}
+        >
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <Marker
-            position={[latitud, longitud]}
+            position={[
+              historic.length > 0 && historic[historic.length - 1].latitud,
+              historic.length > 0 && historic[historic.length - 1].longitud
+            ]}
             icon={L.icon({
               iconUrl: userLocation,
               iconSize: [40, 40]
             })}
           >
             <Popup>
-              Latitud: {latitud} <br /> Longitud: {longitud}
+              Latitud:{" "}
+              {historic.length > 0 && historic[historic.length - 1].latitud}{" "}
+              <br /> Longitud:{" "}
+              {historic.length > 0 && historic[historic.length - 1].longitud}
             </Popup>
           </Marker>
         </Map>
